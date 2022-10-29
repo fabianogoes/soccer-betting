132fa6ebfe782368b08047b83449db72f7d1fffe
Fabiano <fabianogoes@gmail.com>
2022-10-29 18:18:35 -0300

    Configuration start API

--- cmd/main.go
+++ cmd/main.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"log"
-	"world-cup/controllers"
+	"world-cup/api"
+	"world-cup/domain/usecases"
 	"world-cup/initializers"
 )
 
 func main() {
 	// ctx := context.Background()
 
-	log.Println("Setup Application...")
+	log.Println("Load Configurations...")
 	config, _ := initializers.LoadConfig(".")
 
 	// Setup Database: Postgres
+	log.Println("Setup Postgress...")
 	initializers.SetupPostgres(&config)
 
+	// Dependency Injection
+	userUseCase := usecases.NewUserUseCase(initializers.DB)
+	teamUseCase := usecases.NewTeamUseCase(initializers.DB)
+	matchUseCase := usecases.NewMatchUseCase(initializers.DB)
+
 	// Setup Web Api
+	log.Println("Setup Web API...")
 	srv := initializers.SetupHTTPServer(
 		&config,
-		controllers.NewHealthApiHandler(),
-		controllers.NewUserApiHandler(),
+		api.NewHealthApiHandler(),
+		api.NewUserApiHandler(userUseCase),
+		api.NewTeamApiHandler(teamUseCase),
+		api.NewMatchApiHandler(matchUseCase),
 	)
 
 	if err := srv.ListenAndServe(); err != nil {
