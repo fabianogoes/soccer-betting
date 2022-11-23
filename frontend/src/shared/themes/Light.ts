f10ba0e9efd950ffefba753968c11f1de03176db
Fabiano <fabianogoes@gmail.com>
2022-11-23 00:54:31 -0300

    refactoring-frontend: improvements

--- frontend/src/shared/themes/Light.ts
+++ frontend/src/shared/themes/Light.ts
@@ -4,9 +4,9 @@ import { cyan, green, red } from '@mui/material/colors'
 export const LightTheme = createTheme({
   palette: {
     primary: {
-      main: red[700],
-      dark: red[800],
-      light: red[500],
+      main: '#58B792',
+      dark: green[800],
+      light: green[500],
       contrastText: '#ffffff',
     },
     secondary: {
